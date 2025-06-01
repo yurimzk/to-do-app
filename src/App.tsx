70ec5cf025816bdc0f4d1dcabcb98a19ed068567
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-06-01 17:34:10 -0300

    import TodoForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss'
 import { Header } from  './components/Header.tsx'
+import { TodoForm } from  './components/TodoForm.tsx'
 
 function App() {
   return(
     <main>
       <Header />
+      <TodoForm />
     </main>
   )
 }
