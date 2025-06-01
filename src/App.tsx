13c34fddb8350d0ff253d0edca12ba522cc4c1ab
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-06-01 17:08:50 -0300

    create header component and export it to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.scss'
+import { Header } from  './components/Header.tsx'
 
 function App() {
-
+  return(
+    <main>
+      <Header />
+    </main>
+  )
 }
 
 export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return(
+      <div>
+        <h1 className="font-sans text-2xl">To-do List</h1>
+      </div>
+    )
+}
