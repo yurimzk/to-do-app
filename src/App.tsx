a0e17b3e4f5578acb98bbd2be4519fb972ace458
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-06-01 19:06:10 -0300

    Add new task using the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.scss'
+import { useState } from 'react'
 import { Header } from  './components/Header.tsx'
 import { TodoForm } from  './components/TodoForm.tsx'
 
 function App() {
+  const [tasks, setTasks] = useState<string[]>([])
+
+  const handleAddTask = (newTask: string) => {
+    if (!newTask.trim()) return
+    setTasks(prev => [...prev, newTask])
+    // console.log("Tarefa adicionada:", newTask)
+  }
+
   return(
     <main>
       <Header />
-      <TodoForm />
+      <TodoForm onAddTask={handleAddTask} />
     </main>
   )
 }
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,10 +1,29 @@
-export function TodoForm() {
+import React, { useState } from 'react'
+
+interface TodoFormProps {
+  onAddTask: (text: string) => void
+}
+
+export function TodoForm({ onAddTask }: TodoFormProps) {
+  const [input, setInput] = useState ("")
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    onAddTask(input)
+    setInput("")
+  }
+
   return(
-    <div>
-      <input type="text" id="form" name="form" placeholder="New task" className="border-2 rounded-lg" />
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+      name="form"
+      placeholder="New task"
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      className="form- border-2 rounded-lg" />
       <button type="submit" className="border-2 rounded-lg">
         +
       </button>
-    </div>
+    </form>
   )
 }
