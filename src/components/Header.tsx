4127dab07250ba2dc50d37ffc01ba3d6f82d2a7a
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-06-03 20:30:17 -0300

    fix: correct typo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
     return(
-      <div  className="flex justify-center bg-gray-300 p-6 mb-4">
+      <div className="flex justify-center bg-gray-300 p-6 mb-4">
         <h1 className="font-sans text-2xl text-gray-800">To-do App</h1>
       </div>
     )
