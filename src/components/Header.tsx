df57d3bb7942bcdc73d0c580597782b38b8355d8
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-06-03 20:28:50 -0300

    refactor: change heading title and add style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
     return(
-      <div>
-        <h1 className="font-sans text-2xl">To-do List</h1>
+      <div  className="flex justify-center bg-gray-300 p-6 mb-4">
+        <h1 className="font-sans text-2xl text-gray-800">To-do App</h1>
       </div>
     )
 }
