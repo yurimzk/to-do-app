644dba1234def0595db74f0a26b06bf3941ce6df
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-06-03 20:29:24 -0300

    add basic spacing style to to-do list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,11 +9,11 @@ type TodoListProps = {
 
 export function TodoList({ tasks }: TodoListProps) {
   return (
-    <ul>
+    <ul className="flex flex-col gap-1 ">
       {tasks.map((task) => (
-        <li key={task.id}>
+        <li key={task.id} className="flex justify-between text-gray-800" >
           {task.name}
-          <input type="checkbox" name={task.name} id={task.id} />
+          <input type="checkbox" name={task.name} id={task.id} className="w-4 h-4" />
         </li>
       ))}
     </ul>
