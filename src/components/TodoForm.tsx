90612254e7bc660bf53bf2556bd5639d1c103301
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-06-03 21:08:46 -0300

    feat: style input form and button

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Plus } from 'phosphor-react'
 
 interface TodoFormProps {
   onAddTask: (text: string) => void
@@ -14,15 +15,15 @@ export function TodoForm({ onAddTask }: TodoFormProps) {
   }
 
   return(
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="flex justify-center align-middle gap-2 mt-8">
       <input type="text"
       name="form"
       placeholder="New task"
       value={input}
       onChange={(e) => setInput(e.target.value)}
-      className="form- border-2 rounded-lg" />
-      <button type="submit" className="border-2 rounded-lg">
-        +
+      className="border-2 rounded-lg px-1 py-0.5 placeholder-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-800 hover:ring-1 hover:ring-gray-800" />
+      <button type="submit" className="w-6 h-6 border-2 rounded-full border-gray-300 flex justify-center self-center opacity-60 hover:opacity-100">
+        <Plus className="w-4 h-4 self-center" />
       </button>
     </form>
   )
